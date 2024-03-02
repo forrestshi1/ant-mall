e9c569d25dfbcf80b859d1bb9d2ded49a05006c7
forrestshi1 <forretshi802@gmail.com>
2024-03-02 20:57:43 +0800

    添加product模块

---
+++ src/main/java/com/example/antmall/business/product/service/ProductService.java
@@ -0,0 +1,8 @@
+package com.example.antmall.business.product.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.example.antmall.business.product.entity.Product;
+
+public interface ProductService extends IService<Product> {
+
+}
