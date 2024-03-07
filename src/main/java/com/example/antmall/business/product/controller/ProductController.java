c075ea8d5e5872a1faa3f0f51ec6a094c24a0b21
forrestshi1 <forretshi802@gmail.com>
2024-03-07 17:20:46 +0800

    实现分页查询

--- src/main/java/com/example/antmall/business/product/controller/ProductController.java
+++ src/main/java/com/example/antmall/business/product/controller/ProductController.java
@@ -2,14 +2,14 @@ package com.example.antmall.business.product.controller;
 
 import com.example.antmall.business.product.bo.ProductAddBO;
 import com.example.antmall.business.product.bo.ProductEditBO;
+import com.example.antmall.business.product.bo.ProductQueryBO;
 import com.example.antmall.business.product.service.ProductService;
+import com.example.antmall.business.product.vo.ProductQueryVO;
+import com.example.antmall.common.entity.PageVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Api(tags = "商品管理")
 @RestController
@@ -27,6 +27,12 @@ public class ProductController {
     @PostMapping("edit")
     public void edit(@RequestBody ProductEditBO editBO) {
         productService.edit( editBO);
+    }
+
+    @ApiOperation("分页查询")
+    @GetMapping("page")
+    public PageVO<ProductQueryVO> page(@RequestBody ProductQueryBO queryBO) {
+        return  productService.page(queryBO);
     }
 
 }
--- src/main/java/com/example/antmall/business/product/service/ProductService.java
+++ src/main/java/com/example/antmall/business/product/service/ProductService.java
@@ -3,10 +3,17 @@ package com.example.antmall.business.product.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.example.antmall.business.product.bo.ProductAddBO;
 import com.example.antmall.business.product.bo.ProductEditBO;
+import com.example.antmall.business.product.bo.ProductQueryBO;
 import com.example.antmall.business.product.entity.Product;
+import com.example.antmall.business.product.vo.ProductQueryVO;
+import com.example.antmall.common.entity.PageVO;
+import org.springframework.web.bind.annotation.RequestBody;
 
 public interface ProductService extends IService<Product> {
     void add(ProductAddBO addBO);
     void edit(ProductEditBO editBO);
 
+    PageVO<ProductQueryVO> page(ProductQueryBO queryBO);
+
 }
+
